95968ced32d1156211c60db0cf4e92da486f737d
Krzysiek102 <Krzysztof.Kurek102@gmail.com>
2016-04-14 22:36:02 +0200

    implementation of seasons 1986 - current

--- allTimeTableGenerator.js
+++ allTimeTableGenerator.js
@@ -1,17 +1,38 @@
 var PointsCalculator = function  (){
     
 };
+PointsCalculator.prototype.POINTS_FOR_DEFEAT = 0;
+PointsCalculator.prototype.POINTS_FOR_DRAW = 1;
+PointsCalculator.prototype.POINTS_FOR_WIN_OLD = 2;
+PointsCalculator.prototype.POINTS_FOR_WIN_NEW = 3;
+PointsCalculator.prototype.POINTS_FOR_DECISIVE_WIN_OLD = 3;
+PointsCalculator.prototype.POINTS_FOR_DECISIVE_DEFEAT_OLD = -1;
 
 PointsCalculator.prototype.GetPointsForTheGame = function(seasonStartYear, goalsScored, goalsLost){
     if (seasonStartYear >=1995)
     {
         if (goalsScored>goalsLost){
-            return 3;
+            return this.POINTS_FOR_WIN_NEW;
+        }
+        if (goalsScored === goalsLost){
+            return this.POINTS_FOR_DRAW;
+        }else{ //if goalsScored<goalsLost
+            return this.POINTS_FOR_DEFEAT;
+        }
+    }else if (seasonStartYear>=1986 && seasonStartYear <=1989){
+        if (goalsScored>=goalsLost+3){
+            return this.POINTS_FOR_DECISIVE_WIN_OLD;
+        } else if (goalsScored>goalsLost){
+            return this.POINTS_FOR_WIN_OLD;
         }else if (goalsScored === goalsLost){
-            return 1;
-        }else{//if (goalsScored < goalsLost)
-            return 0;
-        } 
+            return this.POINTS_FOR_DRAW;
+        }
+        else if (goalsScored+3 <= goalsLost){
+            return this.POINTS_FOR_DECISIVE_DEFEAT_OLD;
+        }else {//goalsScored<goalsLost && goalsScored+3>goalsLost
+            return this.POINTS_FOR_DEFEAT;
+        }
     }
+    
     throw "Not implemented";
 } 
--- qunit/allTimeTableGeneratorSpecQUnit.js
+++ qunit/allTimeTableGeneratorSpecQUnit.js
@@ -1,19 +1,49 @@
 /// <reference path="../typings/qunit/qunit.d.ts" />
 
+var pointsCalculator;
 
-module('Points calculator');
+module('Points calculator', {
+    setup: function  (){
+        pointsCalculator = new PointsCalculator();    
+    },
+});
 
-test('1:0 win in season 2015/2016 should give 3 points', function () {
-    
-    //arrange
-    var pointsCalculator = new PointsCalculator();
-    var seasonStartYear = 2015;
-    var goalsScored = 1;
-    var goalsLost = 0;
-    
-    //act
-    var totalAmountOfPoints = pointsCalculator.GetPointsForTheGame(seasonStartYear, goalsScored, goalsLost);
-    
-    //assert
+test('1:0 in season 2015/2016 should give 3 points', function () {
+    var totalAmountOfPoints = pointsCalculator.GetPointsForTheGame(2015, 1, 0);
     strictEqual(totalAmountOfPoints, 3);
+});
+
+test('1:1 in season 2015/2016 should give 1 point', function  (){
+    var totalAmountOfPoints = pointsCalculator.GetPointsForTheGame(2015, 1, 1);
+    strictEqual(totalAmountOfPoints, 1);    
+});
+
+test('0:1 in season 2015/2016 should give 0 points', function  (){
+    var totalAmountOfPoints = pointsCalculator.GetPointsForTheGame(2015, 0, 1);
+    strictEqual(totalAmountOfPoints, 0);    
+});
+
+test('1:0 in season 1986/1987 should give 2 points', function  (){
+    var totalAmountOfPoints = pointsCalculator.GetPointsForTheGame(1986, 1, 0);
+    strictEqual(totalAmountOfPoints, 2);    
+});
+
+test('3:0 in season 1986/1987 should give 3 points', function  (){
+    var totalAmountOfPoints = pointsCalculator.GetPointsForTheGame(1986, 3, 0);
+    strictEqual(totalAmountOfPoints, 3);    
+});
+
+test('1:1 in season 1986/1987 should give 1 point', function  (){
+    var totalAmountOfPoints = pointsCalculator.GetPointsForTheGame(1986, 1, 1);
+    strictEqual(totalAmountOfPoints, 1);    
+});
+
+test('0:1 in season 1986/1987 should give 0 points', function  (){
+    var totalAmountOfPoints = pointsCalculator.GetPointsForTheGame(1986, 0, 1);
+    strictEqual(totalAmountOfPoints, 0);    
+});
+
+test('0:3 in season 1986/1987 should give -1 points', function  (){
+    var totalAmountOfPoints = pointsCalculator.GetPointsForTheGame(1986, 0, 3);
+    strictEqual(totalAmountOfPoints, -1);    
 });
