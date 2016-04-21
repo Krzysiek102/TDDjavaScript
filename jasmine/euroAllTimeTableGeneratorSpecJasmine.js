2675e8ab0c442f3201a0416f79c1410be1627095
Krzysiek102 <Krzysztof.Kurek102@gmail.com>
2016-04-21 07:45:10 +0200

    Jasmine specs

---
+++ jasmine/euroAllTimeTableGeneratorSpecJasmine.js
@@ -0,0 +1,23 @@
+/// <reference path="../typings/jasmine/jasmine.d.ts" />
+'use strict';
+
+describe('Points calculator', function () {
+    var pointsCalculator;
+    
+    beforeEach(function () {
+        pointsCalculator = new PointsCalculator();  
+    });
+    
+    it('should give 3 points for winning 4:0 in 2008', function () {
+        var totalAmountOfPoints = pointsCalculator.GetPointsForResult(2008, 3, 0);
+        expect(totalAmountOfPoints).toBe(3);
+    });
+    
+    it('should give 2 for winning 4:0 in 1992', function () {
+        var totalAmountOfPoints = pointsCalculator.GetPointsForResult(1992, 3, 0);
+        expect(totalAmountOfPoints).toBe(2);
+    });
+});
+
+
+    
