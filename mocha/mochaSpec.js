67556dbe4bf9e997fe6bb27e01fd868736f1954b
Krzysiek102 <Krzysztof.Kurek102@gmail.com>
2016-04-21 08:27:39 +0200

    mocha

---
+++ mocha/mochaSpec.js
@@ -0,0 +1,21 @@
+///reference path="../typings/mocha/mocha.d.ts"
+///reference path="../typings/chai/chai.d.ts"
+'use strict';
+
+describe('Points calculator', function () {
+    var pointsCalculator;
+
+    beforeEach(function () {
+        pointsCalculator = new PointsCalculator();
+    });
+
+    it('should give 3 points for winning 4:0 in 2008', function () {
+        var totalAmountOfPoints = pointsCalculator.GetPointsForResult(2008, 3, 0);
+        chai.expect(totalAmountOfPoints).to.equal(3);
+    });
+
+    it('should give 2 for winning 4:0 in 1992', function () {
+        var totalAmountOfPoints = pointsCalculator.GetPointsForResult(1992, 3, 0);
+        chai.expect(totalAmountOfPoints).to.equal(2);
+    });
+});
